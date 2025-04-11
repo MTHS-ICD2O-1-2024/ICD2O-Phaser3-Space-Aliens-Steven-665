d1f3b658f0ea4386f75400780b85effc3e666dd1
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-11 17:12:09 +0000

    game.js added

--- js/game.js
+++ js/game.js
@@ -3,3 +3,13 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  length: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
