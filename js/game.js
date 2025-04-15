81bad88ac7992a9553d736f8164f430c877c8603
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-04-15 17:09:42 +0000

    added js

--- js/game.js
+++ js/game.js
@@ -1,14 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yi.zhou
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
 
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   length: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade:{
+      debug: true
+    }
+  },
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page.
+    autoCenter: Phaser.Scale.Center_Both,
+  }
 }
 
 const game = new Phaser.Game(config)
