45a548a9447e922cac4c1ebe3b4c51180e2f1f95
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-05-26 16:59:57 +0000

    add bomb

--- js/gameScene.js
+++ js/gameScene.js
@@ -48,6 +48,7 @@ class GameScene extends Phaser.Scene {
     // sound
     this.load.audio("laser", "./assets/laser1.wav")
     this.load.audio("explosion", "./assets/barrelExploding.wav")
+    this.load.audio("bomb", "./assets/bomb.wav")
 
   }
 
@@ -75,6 +76,17 @@ class GameScene extends Phaser.Scene {
       this.scoreText.setText('Score: ' + this.score.toString())
       this.createAlien()
       this.createAlien()
+    }.bind(this))
+
+    // Collisions between ship and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function(shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true})
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
     }.bind(this))
   }
 
